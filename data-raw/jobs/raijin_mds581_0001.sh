295825a5f54d8bc3900cae2788d07212007297da
Michael Sumner <mdsumner@gmail.com>
2016-06-06 20:30:14 +1000

    trygain

--- data-raw/jobs/raijin_mds581_0001.sh
+++ data-raw/jobs/raijin_mds581_0001.sh
@@ -6,5 +6,4 @@
 #PBS -l jobfs=1GB
 #PBS -l ncpus=1
 #PBS -l software=r_netcdf.sh
-#PBS -l /home/581/mds581/CMIP5files/data-raw/jobs # The job will be executed from current working directory instead of home.
 ./rspawn.sh  > r_netcdf_0001.out
