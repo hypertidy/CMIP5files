d562292ac4cdf59ed68743dbfef0c0287fdaf3f6
Michael Sumner <mdsumner@gmail.com>
2016-06-06 20:21:08 +1000

    more test

--- data-raw/jobs/r_netcdf_0001.r
+++ data-raw/jobs/r_netcdf_0001.r
@@ -1,4 +1,4 @@
-# module load         proj/4.9.0  netcdf/4.3.3.1p  
+
 dp <- "/g/data/ua6/unofficial-ESG-replica/tmp/tree/tds.ucar.edu/thredds/fileServer/datazone/cmip5_data/cmip5/output1/NCAR/CCSM4/rcp85/mon/ocean/Omon/r6i1p1/v20140820/tauuo"
 library(raster)
 f <- list.files(dp, full.names = TRUE)
@@ -7,3 +7,4 @@ library(angstroms)
 nc <- NetCDF(f)
 
 print(nc$variable)
+saveRDS(nc, file = file.path("nc", sprintf("%s.rds", nc$file$filename)))
